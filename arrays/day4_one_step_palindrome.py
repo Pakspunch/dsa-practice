a8d29566479ad459807e48eb9f68d2aef8d166e3
Pratik John Surin <56755605+Pakspunch@users.noreply.github.com>
2026-02-09 23:24:47 +0530

    Add one-step palindrome checker functionality
    Implement palindrome checker allowing one deletion.

---
+++ arrays/day4_one_step_palindrome.py
@@ -0,0 +1,48 @@
+# Palindrome checker with one deletion step
+
+def is_palindrome_range(left, right, input_array):
+    while left < right:
+        if input_array[left] != input_array[right]:
+            return False
+        left += 1 
+        right -= 1 
+    return True
+
+def is_palindrome(input_array):
+    left = 0
+    right = len(input_array) - 1 
+    
+    while left < right:
+        if input_array[left] != input_array[right]:
+            return is_palindrome_range(left + 1, right, input_array) or is_palindrome_range(left, right - 1, input_array)
+        left += 1 
+        right -= 1 
+    return True
+
+def convert_string(input_string):
+    input_array = list(input_string)
+    usable_array = []
+    for element in input_array:
+        if element.isalnum():
+            usable_array.append(element.lower())
+    return usable_array
+
+def is_Empty(input_array):
+    return len(input_array) == 0
+
+def print_answer(input_string):
+    #convert the string to usable input
+    usable_array = convert_string(input_string)
+    
+    #check for empty String
+    if is_Empty(usable_array):
+        print("The string is Empty")
+    else:
+        #check for palindrome
+        if is_palindrome(usable_array):
+            print(input_string, ": Is a Palindrome")
+        else:
+            print(input_string, ": Is not a Palindrome")
+    
+String1 = "abca"
+print_answer(String1)
